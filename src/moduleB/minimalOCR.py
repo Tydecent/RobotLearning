1e1dc089f3fe797f7054ba75ddc78ea9bfa1a741
Tydecent <Smart100430@163.com>
2026-06-23 11:09:29 +0800

    fix(src/moduleB/minimalOCR): 修正图片路径以适应相对路径
    - 修正了 `result = reader.readtext("/home/a24/project/RobotLearning/src/moduleB/samples/test.png", detail=0)` 中的图片路径，改为使用相对路径 `./samples/test.png`。这样可以确保代码在不同环境中都能正确读取图片文件。

--- src/moduleB/minimalOCR.py
+++ src/moduleB/minimalOCR.py
@@ -1,7 +1,7 @@
 import easyocr
 
 reader = easyocr.Reader(["ch_sim", "en"])
-result = reader.readtext("/home/a24/project/RobotLearning/src/moduleB/samples/test.png", detail=0)
+result = reader.readtext("./samples/test.png", detail=0)
 #如果不加detail=0，则result是一个列表，每个元素是一个元组，元组中第一个元素是文本，第二个元素是文本的坐标
 # 后面这一大串列表就是 reader.readtext(...) 的原始返回值。EasyOCR 默认 detail=1，所以每个识别结果都是一个三元组：
 
